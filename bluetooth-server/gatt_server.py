1f5a0cd333fe656bc75e2f90bf1d6596d92fbf39
bihut <angelrzafra@gmail.com>
2024-05-14 17:35:03 +0200

    First commit

--- bluetooth-server/gatt_server.py
+++ bluetooth-server/gatt_server.py
@@ -42,9 +42,9 @@ class Application(dbus.service.Object):
         self.services = []
         dbus.service.Object.__init__(self, bus, self.path)
         self.add_service(HeartRateService(bus, 0))
-        self.add_service(BatteryService(bus, 1))
-        self.add_service(TestService(bus, 2))
-        self.add_service(WifiService(bus,3))
+        #self.add_service(BatteryService(bus, 1))
+        #self.add_service(TestService(bus, 2))
+        self.add_service(WifiService(bus,1))
         self.localname = "cardmed-"+str(id)
     def get_path(self):
         return dbus.ObjectPath(self.path)
